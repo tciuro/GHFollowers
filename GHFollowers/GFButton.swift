5008430b73227d45ecc1e473333778d09309d56a
Tito Ciuro <tito@webbo.com>
2020-01-04 16:35:28 -0800

    Custom Views - UIButton & UITextField

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Tito Ciuro on 1/4/20.
+//  Copyright © 2020 Tito Ciuro. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10.0
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,40 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by Tito Ciuro on 1/4/20.
+//  Copyright © 2020 Tito Ciuro. All rights reserved.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10.0
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12.0
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+    }
+}
