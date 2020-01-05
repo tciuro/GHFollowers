f455334e16c587ab56e3f3fb29c55a37786fa3bc
Tito Ciuro <tito@webbo.com>
2020-01-04 16:57:29 -0800

    Search Screen UI (Programmatic)

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -9,11 +9,57 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureLogoView()
+        configureTextField()
+        configureCallToActionButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    private func configureLogoView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80.0),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200.0),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200.0)
+        ])
+    }
+    
+    private func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48.0),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50.0),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50.0),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+    }
+    
+    private func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
 
-        view.backgroundColor = .systemPink
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50.0),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50.0),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50.0),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
     }
 
 }
