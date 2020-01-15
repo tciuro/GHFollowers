e34e68674d0ab79e44f5e546ed6debf9ae16b4ba
Tito Ciuro <tito@webbo.com>
2020-01-14 18:22:12 -0800

    Contrain the activity indicator to the container view

--- GHFollowers/Extensions/UIViewController+Extension.swift
+++ GHFollowers/Extensions/UIViewController+Extension.swift
@@ -36,8 +36,8 @@ extension UIViewController {
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
         ])
         
         activityIndicator.startAnimating()
