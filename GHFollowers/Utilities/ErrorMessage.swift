40bb0d1b4ccf7fe23f24a411e07df573c2853a33
Tito Ciuro <tito@webbo.com>
2020-01-19 17:50:36 -0800

    Cleanup message

--- GHFollowers/Utilities/ErrorMessage.swift
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum GFError: String, Error {
-    case invalidUsername = "This username created an unvalid request. please try again."
+    case invalidURL = "Unable to generate a valid URL. Please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
