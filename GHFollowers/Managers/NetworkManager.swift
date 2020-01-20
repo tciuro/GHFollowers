00115e930a930b7ce0758bc5dfc27d2d81b91f94
Tito Ciuro <tito@webbo.com>
2020-01-19 17:51:23 -0800

    Refactor and cleanup NetworkManager

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -13,46 +13,45 @@ class NetworkManager {
     
     private var imageCache = NSCache<NSString, UIImage>()
 
-    private let baseURL = "https://api.github.com"
+    private var baseGitHubURL: URLComponents {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "api.github.com"
+        return urlComponents
+    }
 
     private init() {}
     
     func getFollowers(for username: String, page: Int, completion: @escaping (Result<[Follower], GFError>) -> Void) {
-        let endpoint = baseURL + "/users/\(username)/followers?per_page=100&page=\(page)"
+        guard let url = _gitHubURL(with: "/users/\(username)/followers", query: ["per_page": "100", "page": String(page)]) else {
+                completion(.failure(.invalidURL))
+                return
+        }
         
-        guard let url = URL(string: endpoint) else {
-            completion(.failure(.invalidUsername))
+        _sendGHRequest(to: url, modelType: [Follower].self) { result in
+            switch result {
+            case .success(let user):
+                completion(.success(user))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func getUserInfo(for username: String, completion: @escaping (Result<User, GFError>) -> Void) {
+        guard let url = _gitHubURL(with: "/users/\(username)") else {
+            completion(.failure(.invalidURL))
             return
         }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                completion(.failure(.unableToComplete))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completion(.failure(.invalidResponse))
-                return
-            }
-            
-            guard let data = data else {
-                completion(.failure(.invalidData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let followers = try decoder.decode([Follower].self, from: data)
-                completion(.success(followers))
-            } catch {
-                completion(.failure(.failedToBuildFollowerList))
-                return
+
+        _sendGHRequest(to: url, modelType: User.self) { result in
+            switch result {
+            case .success(let user):
+                completion(.success(user))
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
-        
-        task.resume()
     }
     
     func downloadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
@@ -79,6 +78,56 @@ class NetworkManager {
             self.imageCache.setObject(image, forKey: urlString as NSString)
             
             completion(image)
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: - Private Section -
+    
+    private func _gitHubURL(with path: String, query: [String: String]? = nil) -> URL? {
+        var urlComponents = baseGitHubURL
+        urlComponents.path = path
+        
+        if let query = query {
+            var queryItems = [URLQueryItem]()
+            
+            for (key, value) in query {
+                queryItems.append(URLQueryItem(name: key, value: value))
+            }
+            
+            urlComponents.queryItems = queryItems
+        }
+
+        return urlComponents.url
+    }
+    
+    private func _sendGHRequest<T: Codable>(to url: URL, modelType: T.Type, completion: @escaping (Result<T, GFError>) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let model = try decoder.decode(T.self, from: data)
+                completion(.success(model))
+            } catch {
+                completion(.failure(.failedToBuildFollowerList))
+                return
+            }
         }
         
         task.resume()
