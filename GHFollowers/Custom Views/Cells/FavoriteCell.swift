3142c6919bf0bb8eaea689adc8a1ebfeaf7322ac
Tito Ciuro <tito@webbo.com>
2020-01-26 11:06:36 -0800

    UITableView - Favorites Cell

---
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -0,0 +1,58 @@
+//
+//  FavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Tito Ciuro on 1/26/20.
+//  Copyright © 2020 Tito Ciuro. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+
+    static let reuseID = "FavoriteCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .left, fontSize: 26.0)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(favorite: Follower) {
+        usernameLabel.text = favorite.login
+        NetworkManager.shared.downloadImage(from: favorite.avatarUrl.absoluteString, completion: { [weak self] avatarImage in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.avatarImageView.image = avatarImage
+            }
+        })
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType = .disclosureIndicator
+        
+        let padding: CGFloat = 12.0
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60.0),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60.0),
+            
+            usernameLabel.topAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24.0),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40.0)
+        ])
+    }
+    
+}
